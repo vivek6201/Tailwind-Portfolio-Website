aac7c4710a8e887a20a06b87a7abcfa28056d6ee
vivek6201 <gvicky4567888@gmail.com>
2023-02-16 23:30:43 +0530

    Building my portfolio website and projects section is completed

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        lightBlue1:"#5194FF",
+        brand:"#322CFF",
+        skyBlue:"#92F6FF"
+      },
+      fontFamily:{
+        rubik:"Rubik,sans-sarif"
+      }
+    },
+  },
+  plugins: [],
+}
